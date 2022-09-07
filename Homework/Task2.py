42b878cf26fb623c06364bda0eeb8db5d0e02754
Starostina <pankovays@mail.ru>
2022-09-07 15:16:29 +0700

    Дописала код

--- Homework/Task2.py
+++ Homework/Task2.py
@@ -1,4 +1,15 @@
 # Напишите программу для проверки истинности утверждения
 #  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
 def predicat_true(x, y, z):
     print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} is {(not (x or y or z)) == (not x and not y and not z)}')
+
+
+predicat_true(0, 0, 0)
+predicat_true(0, 0, 1)
+predicat_true(0, 1, 0)
+predicat_true(0, 1, 1)
+predicat_true(1, 0, 0)
+predicat_true(1, 0, 1)
+predicat_true(1, 1, 0)
+predicat_true(1, 1, 1)
